48b41478337a5ef4701c680e0d4748b73225b116
Lloyd <zebra05@gmail.com>
2022-11-09 21:53:39 +0200

    Correct typo

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   
   def index
-    @movies = ["Iron Man", "Superman", "Spider Man"]
+    @movies = ["Iron Man", "Superman", "Spider-Man"]
   end
 end
