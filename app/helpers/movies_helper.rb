8f5d3749819371effcdc5a8203cb2f85dc70bb1a
Lloyd <zebra05@gmail.com>
2022-11-12 22:21:39 +0200

    Change the total_gross helper to call the flop? method

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,10 +1,6 @@
 module MoviesHelper
   
   def total_gross(movie)
-    if movie.total_gross < 225_000_000
-      "Flop"
-    else
-      number_to_currency(movie.total_gross, precision: 0)
-    end
+    movie.flop? ? "Flop" : number_to_currency(movie.total_gross, precision: 0)
   end
 end
