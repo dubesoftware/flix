87035e2faf4e4f3f5a11b760e68d12b5ae760ba5
Lloyd <zebra05@gmail.com>
2022-11-16 13:07:06 +0200

    Create a hash of permitted attributes and pass that hash as a parameter to the new method of the Movie class

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -26,7 +26,10 @@ class MoviesController < ApplicationController
   end
   
   def create
-    @movie = Movie.new(params[:movie])
+    movie_params = 
+      params.require(:movie).
+        permit(:title, :description, :rating, :released_on, :total_gross)
+    @movie = Movie.new(movie_params)
     @movie.save
     redirect_to @movie
   end
