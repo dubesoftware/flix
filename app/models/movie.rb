d37fd22f4b9c12accf10b96bc89183db241c8d1f
Lloyd <zebra05@gmail.com>
2022-11-17 13:35:33 +0200

    Define hits class method to fetch movies with at least 00M total gross, ordered with the highest grossing movie first

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,6 +4,10 @@ class Movie < ApplicationRecord
     where("released_on < ?", Time.now).order("released_on desc")
   end
   
+  def self.hits
+    where("total_gross >= 300000000").order(total_gross: :desc)
+  end
+  
   def flop?
     total_gross.blank? || total_gross < 225_000_000
   end
