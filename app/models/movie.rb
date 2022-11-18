4f4991c2fb5c6132bbae2c6a642f645617c5c93c
Lloyd <zebra05@gmail.com>
2022-11-18 15:25:27 +0200

    Validate that the total_gross field must be a number greater than or equal to 0

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,7 @@ class Movie < ApplicationRecord
   
   validates: :title, :released_on, :duration, presence: true
   validates: :description, length: { minimum: 25 }
+  validates: :total_gross, numericality: { greater_than_or_equal_to: 0 }
   
   def self.released
     where("released_on < ?", Time.now).order("released_on desc")
