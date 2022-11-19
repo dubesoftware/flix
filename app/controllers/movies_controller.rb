81aa693d3840e06506cdf3d363892940f09d266a
Lloyd <zebra05@gmail.com>
2022-11-19 03:05:53 +0200

    Example usage of danger flash type

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -37,7 +37,8 @@ class MoviesController < ApplicationController
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
-    redirect_to movies_url, status: :see_other, alert: "Movie successfully deleted!"
+    redirect_to movies_url, status: :see_other, danger: "I'm sorry, Dave, I'm afraid I can't do that!"
+    # alert: "Movie successfully deleted!"
   end
   
   private
