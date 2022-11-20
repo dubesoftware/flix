03278bcfce08da7d2b019bce3ed0ed22159cf80c
Lloyd <zebra05@gmail.com>
2022-11-20 04:08:56 +0200

    Cleanup duplication using before_action filter and set_movie private method of ReviewController

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,16 @@
 class ReviewsController < ApplicationController
   
+  before_action :set_movie
+  
   def index
-    @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
   end
   
   def new
-    @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.new
   end
   
   def create
-    @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.new(review_params)
     if @review.save
       redirect_to movie_reviews_url(@movie), notice: "Thanks for your review!"
