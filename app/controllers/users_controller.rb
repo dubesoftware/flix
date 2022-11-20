abe58e670eab18a7bfdaf0e3a8001173cf3635c4
Lloyd <zebra05@gmail.com>
2022-11-21 01:59:19 +0200

    Revert "Implement create action of USersController"
    This reverts commit 0c3a83b09ac9fedbe8d0a580cdafb14691eb84f2.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,13 +11,4 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
-  def create
-    @user = User.new(params[:user])
-    if @user.save
-      redirect_to user_url(@user), notice: "Thanks for signing up!"
-    else
-      render "new", status: :unprocessable_entity
-    end
-  end
 end
