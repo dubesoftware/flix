03e6250702e824d255f134a883f076d4e49779db
Lloyd <zebra05@gmail.com>
2022-11-20 06:52:43 +0200

    Change the average_stars helper method to display the average number of stars as a number of asterisks

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -12,7 +12,7 @@ module MoviesHelper
     if movie.average_stars.zero?
       content_tag(:strong, "No reviews")
     else
-      pluralize(number_with_precision(movie.average_stars, precision: 1), "star")
+      "*" * movie.average_stars.round
     end
   end
 end
