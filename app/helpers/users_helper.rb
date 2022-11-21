cb210f6f7a7230f677012e2809709b821a7fefa0
Lloyd <zebra05@gmail.com>
2022-11-21 02:24:02 +0200

    Define profile_image custom view helper to generate Gravatar image tag

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  
+  def profile_image(user)
+    gravatar_url = "http://secure.gravatar.com/avatar/#{user.gravatar_id}"
+    image_tag(gravatar_url, alt: user.name)
+  end
 end
