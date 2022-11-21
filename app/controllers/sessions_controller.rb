e40a1cee5c0d8258bacd9d2bb45e89ef0c066288
Lloyd <zebra05@gmail.com>
2022-11-21 18:03:46 +0200

    Revert "Redirect to that user's profile page with a flash notice"
    This reverts commit 61269ae5473eaccee46b4b241bd85d54f4d0c02e.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,6 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to user, flash[:notice] = "Welcome back, Lucy"
     else
     end
   end
