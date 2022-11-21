fa3f083efb53413575db89661dbc69c0657be3b8
Lloyd <zebra05@gmail.com>
2022-11-21 18:02:01 +0200

    If the user is authenticated, record that fact in the session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
     else
     end
   end
