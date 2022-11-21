712312f4b545c3b23f35b877bb8761dcc64a140a
Lloyd <zebra05@gmail.com>
2022-11-21 02:04:24 +0200

    Use more idiomatic redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_url(@user), notice: "Thanks for signing up!"
+      redirect_to @user, notice: "Thanks for signing up!"
     else
       render "new", status: :unprocessable_entity
     end
