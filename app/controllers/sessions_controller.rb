26f87b2134b319e5298909a25db9177cf9d9518f
Lloyd <zebra05@gmail.com>
2022-11-21 18:05:57 +0200

    Redirect to user's profile page with a flash notice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back, #{user.name}!"
     else
     end
   end
