72aaa1a0b0324da0a02e87c5049578eb7af46d75
Lloyd <zebra05@gmail.com>
2022-11-22 02:08:10 +0200

    Clean up duplicate user finding code and rely on set_user private method in specific actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
   end
   
   def new
@@ -25,11 +24,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to @user, notice: "Account successfully updated!"
     else
@@ -38,7 +35,6 @@ class UsersController < ApplicationController
   end
   
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     session[:user_id] = nil
     redirect_to movies_url, status: :see_other,
