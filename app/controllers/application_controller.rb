e4ba43ee717efc986d92e32a3e56e3cb38d5b0ae
Lloyd <zebra05@gmail.com>
2022-11-22 16:42:06 +0200

    Revert "Move the current_user method from the ApplicationHelper module to the ApplicationController class as a private method and declare it as a helper"
    This reverts commit f52c74a32755411025794039ee071be2c2f3a5f3.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,10 +8,4 @@ class ApplicationController < ActionController::Base
       redirect_to :new_session_url, alert: alert: "Please sign in first!"
     end
   end
-  
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-  
-  helper_method: current_user
 end
