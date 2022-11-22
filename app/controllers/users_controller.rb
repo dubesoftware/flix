22bb85b8b616b5c9084f5368f6057534181b672e
Lloyd <zebra05@gmail.com>
2022-11-22 15:25:27 +0200

    Use except method instead of only method to specify actions to pass to before_action filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, except: [:index, :new, :create]
   
   def index
     @users = User.all
