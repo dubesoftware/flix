b75eec5d5175eabf07abeac7c47f7d5b71277da5
Lloyd <zebra05@gmail.com>
2022-11-22 16:51:18 +0200

    Fix redirect to new_session_url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   
   def require_signin
     unless current_user
-      redirect_to :new_session_url, alert: "Please sign in first!"
+      redirect_to new_session_url, alert: "Please sign in first!"
     end
   end
   
