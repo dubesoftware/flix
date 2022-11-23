adb819b1a139b2195014d5c08b94504b556fa9b7
Lloyd <zebra05@gmail.com>
2022-11-23 15:33:05 +0200

    Require sign in for every action in the ReviewsController

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,7 @@ class ReviewsController < ApplicationController
   
   before_action :set_movie
   before_action :set_review, only: [:edit, :update, :destroy]
+  before_action :require_signin
   
   def index
     @reviews = @movie.reviews
