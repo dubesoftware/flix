3e7e6d24af690346fd6681a5512af615e31e63d1
Lloyd <zebra05@gmail.com>
2022-11-25 17:10:18 +0200

    Implement the create action so that it creates an appropriate favorite and redirects to the movie show page

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,2 +1,8 @@
 class FavouritesController < ApplicationController
+  
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @movie.favourites.create!(user: current_user)
+    redirect_to @movie
+  end
 end
