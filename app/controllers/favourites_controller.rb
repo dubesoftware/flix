630102821790e6dd563f4873eb87c8a353749d96
Lloyd <zebra05@gmail.com>
2022-11-25 18:09:23 +0200

    Define set_movie private method of FavouritesController

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -15,4 +15,10 @@ class FavouritesController < ApplicationController
     movie = Movie.find(params[:movie_id])
     redirect_to movie
   end
+  
+  private
+  
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
+  end
 end
