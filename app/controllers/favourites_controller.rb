069c579252e9e2f847a9a82f059cba5553ca1849
Lloyd <zebra05@gmail.com>
2022-11-25 17:11:31 +0200

    Append current user to movie fans using through association

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -2,7 +2,7 @@ class FavouritesController < ApplicationController
   
   def create
     @movie = Movie.find(params[:movie_id])
-    @movie.favourites.create!(user: current_user)
+    @movie.fans << current_user
     redirect_to @movie
   end
 end
