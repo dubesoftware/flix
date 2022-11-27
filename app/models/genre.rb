042d87e0125012c2e6d7946ffedeffd146c68212
Lloyd <zebra05@gmail.com>
2022-11-27 12:44:03 +0200

    Declare a has_many association with movies that goes through the characterizations association

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,6 @@
 class Genre < ApplicationRecord
   has_many :characterisations, dependent: :destroy
+  has_many :movies, through: :characterisations
   
   validates :name, presence: true, uniqueness: true
 end
