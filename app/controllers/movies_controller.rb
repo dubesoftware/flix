d85ab7c57f8dfa66370898239d211152547643a8
Lloyd <zebra05@gmail.com>
2022-11-29 16:59:23 +0200

    Use movies_filter private method to set @movies instance variable using whitelisted scopes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,18 +5,7 @@ class MoviesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
     
   def index
-    case params[:filter]
-    when "upcoming"
-      @movies = Movie.upcoming
-    when "recent"
-      @movies = Movie.recent
-    when "hits"
-      @movies = Movie.hits
-    when "flops"
-      @movies = Movie.flops
-    else
-      @movies = Movie.released
-    end
+    @movies = Movie.send(movies_filter)
   end
   
   def show
