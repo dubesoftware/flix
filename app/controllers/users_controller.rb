f4799882bbe6748145d54f190ec39905db5532ba
Lloyd <zebra05@gmail.com>
2022-11-29 20:04:21 +0200

    Upate set_user to find user by username (using bang method to raise exception on failure)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
   end
   
   def set_user
-    @user = User.find(params[:id])
+    @user = User.find_by!(username: params[:id])
   end
   
   def require_correct_user
