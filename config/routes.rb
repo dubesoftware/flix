0e774b17ef688bb5f1791d8808a325da43ad8725
Lloyd <zebra05@gmail.com>
2022-11-29 15:20:38 +0200

    Define a new route that uses a :filter parameter to match the URL path movies/filter followed by any scope name and sends the rewuest to the index action of the MoviesController

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     resources :favourites, only: [:create, :destroy]
   end
   
+  get "movies/filter/:filter" => "movies#index"
+  
   resource :session, only: [:new, :create, :destroy]
   
   resources :users
