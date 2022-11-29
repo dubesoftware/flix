9da76c94b0835f11ef842255c22d2ed7a22131c7
Lloyd <zebra05@gmail.com>
2022-11-29 20:12:15 +0200

    Database schema updated by AddSlugToGenres migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_155602) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_181116) do
   create_table "characterisations", force: :cascade do |t|
     t.integer "movie_id", null: false
     t.integer "genre_id", null: false
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_155602) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
   end
 
   create_table "movies", force: :cascade do |t|
