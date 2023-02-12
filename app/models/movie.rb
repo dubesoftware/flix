9735d337c50c74d719092d7ef8c9d3ed9d0ad063
dubesoftware <therealdubesoftware@gmail.com>
2023-02-13 00:46:30 +0200

    Fix indentation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -66,13 +66,13 @@ class Movie < ApplicationRecord
   def acceptable_image
     return unless main_image.attached?
 
-      unless main_image.blob.byte_size <= 1.megabyte
-        errors.add(:main_image, "is too big")
-      end
+    unless main_image.blob.byte_size <= 1.megabyte
+      errors.add(:main_image, "is too big")
+    end
 
-      acceptable_types = ["image/jpeg", "image/png"]
-      unless acceptable_types.include?(main_image.content_type)
-        errors.add(:main_image, "must be a JPEG or PNG")
-      end
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(main_image.content_type)
+      errors.add(:main_image, "must be a JPEG or PNG")
+    end
   end
 end
